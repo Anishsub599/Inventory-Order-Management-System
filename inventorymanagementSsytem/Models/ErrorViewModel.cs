b0fafae74d363cee0ae6d00e3122efacfa04d652
Anish Subedi <anishsubedi599@gmail.com>
2025-08-13 21:55:07 +0545

    basic

---
+++ inventorymanagementSsytem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inventorymanagementSsytem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
